7e3b1e90cb55c697af8c1e75b08c97c75449b029
frankguekeng <frankguekeng11@gmail.com>
2025-05-23 20:31:41 +0100

    Create delegating passwordEncoder

---
+++ src/main/java/com/manning/sbip/ch05/security/CustomAccessDeniedHandler.java
@@ -0,0 +1,18 @@
+package com.manning.sbip.ch05.security;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        response.sendRedirect(request.getContextPath() + "/accessDenied");
+    }
+}
